8884f64b1db1fee4eb8c5861e4801c5a33d27e4f
Nathan Culley <nathan@nathanculley.com>
2026-01-20 07:48:25 -0500

    fix: ensure CNAME file is copied to dist during build

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    emptyOutDir: true,
   }
 })
